f63850497ee2a6a85ac74407c25dd9c30a7e7d6d
Francesco Canessa <makevoid@gmail.com>
2015-04-21 00:58:59 +0100

    Update faucet.rb

--- faucet.rb
+++ faucet.rb
@@ -20,14 +20,16 @@ puts "see #{url = 'https://en.bitcoin.it/wiki/Original_Bitcoin_client/API_calls_
 # import hd private key seed
 
 
- # Checks locally, no api calls, response accurate and very quick
+# Checks locally, no api calls, response accurate and very quick
+
 
 
 ### blockchain send
 
- # A thing very difficult to do without the blockchain, sign and push the transaction
+# A thing very difficult to do without the blockchain, sign and push the transaction
 
 # You dont have to pipushtx, you just need configured networking
+
 
 
 ### secure the wallet with (optional but good, it's like putting a lock pin, except you should put a wrong password and adds a timed access/log-in in to your app, bitcoind in this case, to release the funds)
