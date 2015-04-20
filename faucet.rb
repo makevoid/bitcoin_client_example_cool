823593b71bb3d4d5150d45bd1745360eae53ae47
Francesco Canessa <makevoid@gmail.com>
2015-04-21 00:42:59 +0100

    Biwi code
    Faucet can be made anyway because is a cool project

--- faucet.rb
+++ faucet.rb
@@ -4,3 +4,31 @@ require_relative "./config/env"
 
 BC = BitcoinClient::Client.new 'bitcoin', RPC_PASSWORD
 puts BC.balance
+
+
+### blockchain watcher - used in BiWi
+
+# checkaddressbalance
+
+# multisig_address.derive
+
+# dumpprivker (manually CLI)
+# import hd private key seed
+
+
+Checks locally, no api calls, response accurate and very quick
+
+
+### blockchain send
+
+A thing very difficult to do without the blockchain, sign and push the transaction
+
+You dont have to pipushtx, you just need configured networking
+
+
+
+
+
+
+
+
