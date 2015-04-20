da18e40f4babce8f1039abd780810e7271f95e4e
Francesco Canessa <makevoid@gmail.com>
2015-04-21 00:58:05 +0100

    Add walletlock example
    Lol

--- faucet.rb
+++ faucet.rb
@@ -14,6 +14,7 @@ puts "see #{url = 'https://en.bitcoin.it/wiki/Original_Bitcoin_client/API_calls_
 # checkaddressbalance
 
 # multisig_address.derive
+   # BC.addmultisigaddress()
 
 # dumpprivker (manually CLI)
 # import hd private key seed
@@ -29,7 +30,13 @@ puts "see #{url = 'https://en.bitcoin.it/wiki/Original_Bitcoin_client/API_calls_
 # You dont have to pipushtx, you just need configured networking
 
 
+### secure the wallet with (optional but good, it's like putting a lock pin, except you should put a wrong password and adds a timed access/log-in in to your app, bitcoind in this case, to release the funds)
 
+# walletlock
+
+# and 
+
+# walletpassphrase | walletpassphrasechange
 
 
 
